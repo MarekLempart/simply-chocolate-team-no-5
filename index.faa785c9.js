8872c859a45ace6e9f39c4ea3c7548504e85dde4
MarekLempart <144551338+MarekLempart@users.noreply.github.com>
2023-11-20 22:00:43 +0000

    Deploying to gh-pages from @ MarekLempart/simply-chocolate-team-no-5@ae17aacd19644ad61b880a469a907b910410bcde 🚀

---
+++ index.faa785c9.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-subscribe-modal-open]"),closeModalBtn:document.querySelector("[data-subscribe-modal-close]"),modal:document.querySelector("[data-subscribe-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.faa785c9.js.map
